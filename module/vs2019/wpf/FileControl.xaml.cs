08678d091b6e76ba37fa72a5c0d80c9e454e83e2
Easlee Liu <easlee@outlook.com>
2021-07-30 00:08:07 +0800

    close the archive

--- module/vs2019/wpf/FileControl.xaml.cs
+++ module/vs2019/wpf/FileControl.xaml.cs
@@ -32,6 +32,10 @@ namespace oel.archive
         public FileControl()
         {
             InitializeComponent();
+            tbPassword.Visibility = System.Windows.Visibility.Visible;
+            btnOpen.Visibility = System.Windows.Visibility.Visible;
+            btnClose.Visibility = System.Windows.Visibility.Collapsed;
+            spInfo.Visibility = System.Windows.Visibility.Hidden;
         }
 
         private void onOpenClicked(object sender, System.Windows.RoutedEventArgs e)
@@ -45,6 +49,10 @@ namespace oel.archive
             bool result = (bool)openFileDialog.ShowDialog();
             if (!result)
                 return;
+
+            tbPassword.Visibility = System.Windows.Visibility.Hidden;
+            btnOpen.Visibility = System.Windows.Visibility.Collapsed;
+            btnClose.Visibility = System.Windows.Visibility.Visible;
 
             lbEntry.Items.Clear();
             path = openFileDialog.FileName;
@@ -63,6 +71,8 @@ namespace oel.archive
             string entry = lbEntry.SelectedItem as string;
             if (string.IsNullOrEmpty(entry))
                 return;
+
+            spInfo.Visibility = System.Windows.Visibility.Visible;
             tbEntry.Text = entry;
             try
             {
@@ -70,30 +80,61 @@ namespace oel.archive
                 txtViewer.Visibility = entry.EndsWith(".json") ? System.Windows.Visibility.Visible : System.Windows.Visibility.Hidden;
                 if (entry.EndsWith(".png"))
                 {
+                    //不能适用using方式,否则图片的数据会被释放
                     Stream ms = new MemoryStream(reader_.Read(entry));
                     PngBitmapDecoder decoder = new PngBitmapDecoder(ms, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.Default);
-                    imgViewer.ImageSource = decoder.Frames[0];
+                    var frame = decoder.Frames[0];
+                    imgViewer.ImageSource = frame;
+                    tbTip.Text = string.Format("{0}x{1}   {2}", frame.PixelWidth, frame.PixelHeight, formatSize(ms.Length));
                 }
                 else if (entry.EndsWith(".jpg"))
                 {
+                    //不能适用using方式,否则图片的数据会被释放
                     Stream ms = new MemoryStream(reader_.Read(entry));
                     JpegBitmapDecoder decoder = new JpegBitmapDecoder(ms, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.Default);
-                    imgViewer.ImageSource = decoder.Frames[0];
+                    var frame = decoder.Frames[0];
+                    imgViewer.ImageSource = frame;
+                    tbTip.Text = string.Format("{0}x{1}   {2}", frame.PixelWidth, frame.PixelHeight, formatSize(ms.Length));
                 }
-                else if(entry.EndsWith(".json"))
+                else if (entry.EndsWith(".json"))
                 {
-                    using var stream = new MemoryStream();
-                    var document = JsonDocument.Parse(reader_.Read(entry));
-                    var writer = new Utf8JsonWriter(stream, new JsonWriterOptions { Indented = true, Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping});
-                    document.WriteTo(writer);
-                    writer.Flush();
-                    txtViewer.Text = System.Text.Encoding.UTF8.GetString(stream.ToArray());
+                    using (var ms = new MemoryStream())
+                    {
+                        var document = JsonDocument.Parse(reader_.Read(entry));
+                        var writer = new Utf8JsonWriter(ms, new JsonWriterOptions { Indented = true, Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping });
+                        document.WriteTo(writer);
+                        writer.Flush();
+                        txtViewer.Text = System.Text.Encoding.UTF8.GetString(ms.ToArray());
+                        tbTip.Text = string.Format("{0}", formatSize(ms.Length));
+                    }
                 }
             }
-            catch(System.Exception ex)
+            catch (System.Exception ex)
             {
                 Growl.Warning(ex.Message, "StatusGrowl");
             }
+        }
+
+        private void onCloseClicked(object sender, System.Windows.RoutedEventArgs e)
+        {
+            lbEntry.Items.Clear();
+            spInfo.Visibility = System.Windows.Visibility.Hidden;
+            tbPassword.Visibility = System.Windows.Visibility.Visible;
+            btnOpen.Visibility = System.Windows.Visibility.Visible;
+            btnClose.Visibility = System.Windows.Visibility.Collapsed;
+            reader_.Close();
+        }
+
+
+        private string formatSize(long _size)
+        {
+            if (_size < 1024)
+                return string.Format("{0} B", _size);
+            if (_size < 1024 * 1024)
+                return string.Format("{0} KB", _size / 1024);
+            if (_size < 1024 * 1024 * 1024)
+                return string.Format("{0} MB", _size / 1024 / 1024);
+            return string.Format("{0} GB", _size / 1024 / 1024 / 1024);
         }
     }
 }
